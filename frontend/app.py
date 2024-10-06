c4d9702585ef75242cfb8954224ff521b32d7ce0
MattiasKDev <mattias@kerner.ca>
2024-10-06 11:46:30 -0400

    updated app to redirect not found to home

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,16 @@
 import blueprints
-from flask import Flask
+from flask import Flask, redirect
+
+app = Flask(__name__)
+app.register_blueprint(blueprints.home)
+app.register_blueprint(blueprints.systemview)
+app.register_blueprint(blueprints.about)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect("/home")
+
 
 if __name__ == "__main__":
-    app = Flask(__name__)
-    app.register_blueprint(blueprints.home)
-    app.register_blueprint(blueprints.systemview)
-    app.register_blueprint(blueprints.about)
     app.run(debug=True)
